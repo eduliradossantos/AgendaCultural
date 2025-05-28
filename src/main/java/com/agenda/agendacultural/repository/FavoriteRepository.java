f8ff1e45d1b3eef01d259352fc726ad3c7bda8b7
AyresBarbara <AyresBarbara2@gmail.com>
2025-05-28 13:33:14 -0300

    Alteração de configurações

--- src/main/java/com/agenda/agendacultural/repository/FavoriteRepository.java
+++ src/main/java/com/agenda/agendacultural/repository/FavoriteRepository.java
@@ -2,7 +2,6 @@ package com.agenda.agendacultural.repository;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
